70a65054324a532306cbc29ef983f987d4a63ba6
sunamo <radek.jancik@sunamo.cz>
2021-11-08 01:56:34 +0100

    Mass commit 8-11-2021

--- SunamoExceptions5/_/_/Consts.cs
+++ SunamoExceptions5/_/_/Consts.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace SunamoExceptions
-{ 
-public class Consts
 {
-    public const string nulled = "(null)";
+    public class Consts
+    {
+        public const string nulled = "(null)";
         public static string localhostIp = "127.0.0.1";
+        public static byte[] localhostIpBytes = new byte[]{127, 0, 0, 1};
         public const string transformTo = "->";
         public const string Exception = "Exception: ";
         public const string se = "";
