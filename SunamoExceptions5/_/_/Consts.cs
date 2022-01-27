db2de8eadd8f65a1ffa8de52e7a8255cd499fb6b
Radek Jancik <mb@jepsano.net>
2022-01-27 13:05:13 +0100

    .

--- SunamoExceptions5/_/_/Consts.cs
+++ SunamoExceptions5/_/_/Consts.cs
@@ -6,6 +6,7 @@ namespace SunamoExceptions
 {
     public class Consts
     {
+        public const string Schema = "http://schemas.microsoft.com/developer/msbuild/2003";
         public const string nulled = "(null)";
         public static string localhostIp = "127.0.0.1";
         public static byte[] localhostIpBytes = new byte[]{127, 0, 0, 1};
