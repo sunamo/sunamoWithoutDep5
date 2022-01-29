b0215ad0a9ff437c623d07786596e0f4b037edba
Radek Jancik <mb@jepsano.net>
2022-01-29 04:39:11 +0100

    .

---
+++ SunamoExceptions5/_/_/Data/ResultWithException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sunamo.Data
+{
+    public class ResultWithException<T>
+    {
+        public T Data = default(T);
+        /// <summary>
+        /// only string, because Message property isn't editable after instatiate
+        /// </summary>
+        public string exc = null;
+
+        public ResultWithException(T data)
+        {
+            Data = data;
+        }
+
+        public ResultWithException(string exc)
+        {
+            this.exc = exc;
+        }
+
+public ResultWithException()
+        {
+
+        }
+
+
+    }
+}
