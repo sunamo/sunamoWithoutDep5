674f95df17d6f43296e1791b43d9b5b18d9af879
Radek Jancik <mb@jepsano.net>
2022-03-08 08:32:53 +0100

    The Universe, which is the uttered Word of God, is infinite in extent.  There is no empty space beyond creation on any side.  The Universe, which is the Thought of God pronounced,never was not, since God never was inert; nor was, without thinking and creating.  The forms of creation change, the suns and worlds live and die like the leaves and the insects, but the Universe itself is infinite and eternal, because God Is, Was, and Will forever Be, and never did not think and create. - Pike, Albert

--- SunamoExceptions5/_/_/FS.cs
+++ SunamoExceptions5/_/_/FS.cs
@@ -19,6 +19,8 @@ namespace SunamoExceptions
             var ext = FS.GetExtension(relativeTo);
             ext = FS.NormalizeExtension2(ext);
 
+            
+            
             return AllExtensionsHelperWithoutDot.allExtensionsWithoutDot.ContainsKey(ext);
         }
 
