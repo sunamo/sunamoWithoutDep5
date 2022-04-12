22b78821cb61478a14a1b01b1e17cba930dcb4a8
Radek Jancik <mb@jepsano.net>
2022-04-12 22:38:14 +0200

    One may say that evil does not exist for subjective man at all, that there exist only different conceptions of good.  Nobody ever does anything deliberately in the interests of evil, for the sake of evil.  Everybody acts in the interests of good, as he understands it.  But everybody understands it in a different way.  Consequently men drown, slay, and kill one another in the interests of good. - Gurdjieff

--- SunamoExceptions5/_/_/Consts.cs
+++ SunamoExceptions5/_/_/Consts.cs
@@ -8,8 +8,8 @@ namespace SunamoExceptions
     {
         public const string Schema = "http://schemas.microsoft.com/developer/msbuild/2003";
         public const string nulled = "(null)";
-        public static string localhostIp = "127.0.0.1";
-        public static byte[] localhostIpBytes = new byte[]{127, 0, 0, 1};
+        public static string sunamoNetIp = "127.0.0.1";
+        public static byte[] sunamoNetIpBytes = new byte[]{127, 0, 0, 1};
         public const string transformTo = "->";
         public const string Exception = "Exception: ";
         public const string se = "";
